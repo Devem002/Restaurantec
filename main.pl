725755618e41c5e3638fd4519fca1376e142e50f
Devem002 <vega.diego02@gmail.com>
2023-05-25 19:28:18 -0600

    archivo main

---
+++ main.pl
@@ -0,0 +1,50 @@
+%================= Restaurantes ===============%
+
+%Hecho que guarga el nombre del restaurante%
+
+restaurante("McDonlald's").
+restaurante("Taco Bell").
+restaurante("KFC").
+
+restaurante("Limoncello").
+restaurante("Pizzeria ll Pomodoro").
+restaurante("Tsunami Sushi").
+restaurante("El Jardin de Lolita").
+
+restaurante("Leña y Carbon").
+restaurante("El Novillo Alegre").
+restaurante("El Tipico Barrialeño").
+
+%================= Comentario ===============%
+
+%Dispocisiones a tener en cuenta al llegar al lugar%
+
+comentario("McDonlald's", "El restaurante es de ambiente familiar, una vestimenta casual es lo ideal").
+comentario("Taco Bell", "El restaurante aun no esta listo para cumplir con los requisitos sanitarios post-pandemia, por lo que es necesario llevar mascarilla").
+comentario("KFC", "Perfecto restaurnte para ir con la familia, excelente lugar de juegos para los pequeños de la familia").
+
+comentario("Limoncello", "Restaurante de caracter formal, perfecto para degustar un vino y conversar con tranquilidad").
+comentario("Pizzeria ll Pomodoro", "Las mejores pizzas de San Jose, gran lugar para salir en pareja en un ambiente no tan formal").
+comentario("Tsunami Sushi", "El sushi mas conocido del pais, gran lugar para salir con amigos con deliciosa comida asiatica").
+comentario("El Jardin de Lolita", "Centrico y casual, un ambiente tranquilo para salidas, con el agregado de ser pet-friendly").
+
+comentario("Leña y Carbon", "Lugar de alta formalidad, perfecto para almuerzos empresariales, ambiente calmado y con gran atencion").
+comentario("El Novillo Alegre", "Las mejores carnes del país, al mas puro estilo de parrillada argentina, perfecto para ir con compañia").
+comentario("El Tipico Barrialeño", "Lugar casual y familiar, perfecto para disfrutar de cerveza nacional y de un buen partido de futbol").
+
+%================= Menus ===============%
+
+%Menus de los diferentes restaurantes%
+
+menu("McDonlald's").
+menu("Taco Bell").
+menu("KFC").
+
+menu("Limoncello").
+menu("Pizzeria ll Pomodoro").
+menu("Tsunami Sushi").
+menu("El Jardin de Lolita").
+
+menu("Leña y Carbon").
+menu("El Novillo Alegre").
+menu("El Tipico Barrialeño").
